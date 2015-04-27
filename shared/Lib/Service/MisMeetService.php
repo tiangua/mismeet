39cbb75eb6c30fdc734a2b9a7257364681caf68c
Tiangua <tiangua@zju.edu.cn>
2015-04-28 06:10:00 +0800

    add method

--- shared/Lib/Service/MisMeetService.php
+++ shared/Lib/Service/MisMeetService.php
@@ -8,7 +8,15 @@
 namespace MisMeet\Lib\Service;
 
 use OK\PhpEnhance\DomainObject\ServiceResultDO;
+use MisMeet\Lib\DomainObject\ServiceReturnData\MisMeetRes;
 
 class MisMeetService {
-	
+	public function operate($inparam){
+		$resultDO = new ServiceResultDO(true);
+// 		$obj = json_decode($inparam);
+		$dataDO = new MisMeetRes();
+		$dataDO->setRes("operation is " . $inparam);
+		$resultDO->setData($dataDO);
+		return $resultDO;
+	}
 }
