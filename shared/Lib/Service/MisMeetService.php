06c55c49b750e188b8dbc48afc00ad4eb6e5836a
Tiangua <tiangua@zju.edu.cn>
2015-04-29 00:27:19 +0800

    register

--- shared/Lib/Service/MisMeetService.php
+++ shared/Lib/Service/MisMeetService.php
@@ -9,13 +9,32 @@ namespace MisMeet\Lib\Service;
 
 use OK\PhpEnhance\DomainObject\ServiceResultDO;
 use MisMeet\Lib\DomainObject\ServiceReturnData\MisMeetRes;
+use MisMeet\Lib\ConfigConstant\MisMeetErrorEnum;
+use Passport\Lib\Service\AccountService;
+use Passport\Lib\Model\Account;
 
 class MisMeetService {
-	public function operate($inparam){
-		$resultDO = new ServiceResultDO(true);
-// 		$obj = json_decode($inparam);
+	public function operate($inparam) {
+		// param extract
+		if ($obj = json_decode ( $inparam )) {
+			$operation = $obj->op;
+			
+			if ($operation == "reguser"){
+				$accountService = new AccountService();
+				$account = new Account();
+				$account->setUsername("phantom");
+				$account->setPassword("123456");
+				$account->setEmail("phan1@163.com");
+				$accountService ->createAccount($account);
+			}
+		} else {
+			return new ServiceResultDO(false, MisMeetErrorEnum::PARAM_NOTJSON_ERROR);
+		}
+		
+		// build result
+		$resultDO = new ServiceResultDO ( true );		
 		$dataDO = new MisMeetRes();
-		$dataDO->setRes("operation is " . $inparam);
+		$dataDO->setRes("operation is " . $operation);
 		$resultDO->setData($dataDO);
 		return $resultDO;
 	}
